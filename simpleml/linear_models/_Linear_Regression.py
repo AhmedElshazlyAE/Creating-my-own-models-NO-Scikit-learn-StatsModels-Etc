e121109d2a2da30cc8d47de617abf4ae0411f10d
Ahmed Elshazly <ahmedelshazly5262@gmail.com>
2026-01-15 23:14:35 +0200

    Implemented SGD(Stochastic gradient descent) & minor code optimizations
    I noticed that because I was using a small dataset to test my code gradient descent worked fine however it turned out to preform terrible with large dataset due to memory, also due to slow convergence, so I decided to add sgd to the package (psst.. saw the formula thought it was some kind of a medieval spell however it turned out to be just as normal gradient descent you just preform the step calculations on a random subset or batch from the data)

--- simpleml/linear_models/_Linear_Regression.py
+++ simpleml/linear_models/_Linear_Regression.py
@@ -13,13 +13,14 @@ class LinearRegression:
     n = 0  # Data Row Size
     b_count = 0  # Number of params 
 
-    def __init__(self, lr=1e-4, itterations=10**6, optimizer="gradient_descent", patience=5, 
-                 early_stopping=True):
+    def __init__(self, lr=1e-2, itterations=10**5, optimizer="sgd", patience=10, 
+                 early_stopping=True, batch_size=32):
         self.lr = lr
         self.itterations = itterations
         self.optimizer = optimizer
         self.patience = patience
         self.early_stopping = early_stopping
+        self.batch_size = batch_size
         self.fit = LinearRegression.Fit(self)
 
     def beta_dv(self, X, y, y_pred):
@@ -38,7 +39,68 @@ class LinearRegression:
             self.patience = self.model.patience
             self.early_stopping = self.model.early_stopping
             self.beta_dv = self.model.beta_dv
+            self.batch_size = self.model.batch_size
+        
+        
+        def sgd_train(self, X, y):
+            patience_idx = 0
+            best_loss = math.inf  # Highest Possible Loss for early stopping
+            best_prams = self.B
+            self.batch_size = self.batch_size if self.batch_size < len(X) else len(X)
             
+
+            for i in range(self.itterations):
+                start = (i * self.batch_size) % len(X)
+                end = start + self.batch_size
+                y_pred = sum_of_products(self.B, X[start:end])
+                
+                dB = self.beta_dv(X[start:end], y[start:end], y_pred)
+                self.B = opt.gradient_descent(self.B, dB, self.lr) 
+                loss = mean_squared_error(y[start:end], y_pred)
+
+                if round(loss, 4) < round(best_loss, 4):
+                    best_prams = self.B
+                    best_loss = loss
+                    patience_idx = 0
+                else:
+                    patience_idx += 1
+                    
+                if patience_idx + 1 >= self.patience and self.model.early_stopping:
+                    break
+
+
+            print(i)
+
+            return best_prams
+        
+        def adagd_train(self, X, y):
+            G = np.zeros(self.b_count)
+            patience_idx = 0
+            best_loss = math.inf  # Highest Possible Loss for early stopping
+            best_prams = self.B
+            y_pred = sum_of_products(self.B, X)
+            for i in range(self.itterations):
+                dB = self.beta_dv(X, y, y_pred)
+                G += dB ** 2
+                self.B = opt.adaptive_gradient(self.B, dB, G, self.lr)
+
+                y_pred = sum_of_products(self.B, X)
+                loss = mean_squared_error(y, y_pred)
+
+                if round(loss, 3) < round(best_loss, 3):
+                    best_prams = self.B
+                    best_loss = loss
+                    patience_idx = 0
+                else:
+                    patience_idx += 1
+                    
+                if patience_idx + 1 >= self.patience and self.model.early_stopping:
+                    break
+
+            return best_prams
+
+
+
         def __call__(self, X, y):
             self.X = X
             self.y = y
@@ -51,34 +113,11 @@ class LinearRegression:
             self.model.b_count = self.b_count
             self.model.n = self.n
             
-            patience_idx = 0
-            best_loss = math.inf  # Highest Possible Loss for early stopping
-            best_prams = self.B
-            y_pred = sum_of_products(self.B, self.scaled_X)
-            G = np.zeros(self.b_count)  
-            
-            for i in range(self.itterations):
-                dB = self.beta_dv(self.scaled_X, self.y, y_pred)
-                if self.optimizer == "gradient_descent":
-                    self.B = opt.gradient_descent(self.B, dB, self.lr)
-                elif self.optimizer == "adaptive_gradient":
-                    G += dB ** 2
-                    self.B = opt.adaptive_gradient(self.B, dB, G, self.lr)
+            if self.optimizer == "sgd":
+                self.B = self.sgd_train(self.scaled_X, self.y)
+            elif self.optimizer == "adaptive_gradient":
+                self.B = self.adagd_train(self.scaled_X, self.y)
 
-                y_pred = sum_of_products(self.B, self.scaled_X)
-                loss = mean_squared_error(self.y, y_pred)
-
-                if round(loss, 3) < round(best_loss, 3):
-                    best_prams = self.B
-                    best_loss = loss
-                    patience_idx = 0
-                else:
-                    patience_idx += 1
-                    
-                if patience_idx + 1 >= self.patience and self.model.early_stopping:
-                    break
-
-            self.B = best_prams
             return self
             
         def predict(self, X):
